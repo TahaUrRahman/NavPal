6a8bfcd3c74e704c1753e7cc2d92400762e97fbd
Muhammad Hasan Mustafa <hasanm.dev@gmail.com>
2021-12-22 21:55:41 +0500

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private bool jumpKeyPressed;
+    private float horizontalInput;
+    private float verticalInput;
+    private Rigidbody rigidbodyComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbodyComponent = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpKeyPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInput *= 5;
+      
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        if (jumpKeyPressed)
+        {
+            rigidbodyComponent.AddForce(Vector3.up * 3, ForceMode.VelocityChange);
+
+        jumpKeyPressed = false;
+        }
+        rigidbodyComponent.velocity = new Vector3(horizontalInput, rigidbodyComponent.velocity.y , 0);
+     
+    }
+
+    
+}
---
+++ Assets/playerMovements.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovements : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 6f;
+
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float playerSpeed = 2.0f;
+    private float jumpHeight = 1.0f;
+    private float gravityValue = -9.81f;
+
+
+    private void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            Vector3 movDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(movDir.normalized * speed * Time.deltaTime);
+
+            if (Input.GetButtonDown("Jump") && groundedPlayer)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            }
+            playerVelocity.y += gravityValue * Time.deltaTime;
+            controller.Move(playerVelocity * Time.deltaTime);
+        }
+    }
+}
+
